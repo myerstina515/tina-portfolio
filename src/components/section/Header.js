ebd9a35dedf2345b75f21c36e7ec2f60f2453105
TINA MYERS <myers.tina515@gmail.com>
2021-06-15 11:12:37 -0700

    changing portfolio

---
+++ src/components/section/Header.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import HeaderButton from '../layouts/HeaderButton'
+import { header } from '../../profile'
+
+const Header = () => {
+
+    const [mode, setMode] = useState('Dark')
+
+    const scrollTo = () => {
+        window.scrollTo({
+            top: 100000,
+            left: 0,
+            behavior: "smooth"
+        })
+    }
+
+    const toggleDarkMode = () => {
+
+        document.documentElement.classList.toggle('dark-mode')
+        document.getElementById('not-dark').classList.toggle('inverse-dark')
+        document.getElementById('not').classList.toggle('inverse-dark')
+        var x = document.getElementsByClassName('img-pro')
+        for (let i = 0; i < x.length; i += 1) {
+            x.item(i).classList.toggle("inverse-dark");
+        }
+
+        if (document.documentElement.classList.contains('dark-mode')) {
+            setMode('Light')
+            localStorage.setItem('mode', 'Dark')
+        } else {
+            localStorage.setItem('mode', 'Light')
+            setMode('Dark')
+        }
+    }
+
+    return (
+        <div>
+            <div className="Header">
+                <h1>{`I'm ${header.name}`}</h1>
+                <p className="line-1 anim-typewriter">and this is my portfolio... </p>
+                <label className="switch">
+                    <p>{mode}</p>
+                    <input id="mode-switch" onClick={toggleDarkMode} type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+                <HeaderButton />
+            </div>
+            <img id="not-dark" onClick={scrollTo} alt="Contact Me" title="Contact Me" className="gtp" src="profile.png"></img>
+        </div>
+    )
+
+}
+
+export default Header;
---
+++ src/profile.js
@@ -0,0 +1,205 @@
+// Change website animations
+const animation = {
+  // make it false to switch off fade-up animation
+  animate: true,
+  // animation playing duration
+  duration: 750,
+  // if true, animation plays only once when element comes on screen
+  once: false,
+};
+// Change your display name on tha landing display
+const header = {
+  name: "Tina Myers",
+};
+const background = {
+  // Options: Snow or Particle
+  type: "Snow",
+};
+// Write a para about yourself here
+// To update your image, go to './styles/images.css'
+const section2title = "About Me";
+const about = {
+  paragraph:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ",
+};
+// Edit your skill and the percentage you know about it
+// To Add a skill, copy any one below and paste it after the last comma
+const skillsBar = [
+  {
+    name: "HTML5",
+    faClass: "fab fa-html5",
+  },
+  {
+    name: "CSS3",
+    faClass: "fab fa-css3",
+  },
+  {
+    name: "Javascript",
+    faClass: "fab fa-js",
+  },
+  {
+    name: "SASS",
+    faClass: "fab fa-sass",
+  },
+  {
+    name: "Node",
+    faClass: "fab fa-node",
+  },
+  {
+    name: "React",
+    faClass: "fab fa-react",
+  },
+  {
+    name: "Bootstrap",
+    faClass: "fab fa-bootstrap",
+  },
+  {
+    name: "Angular",
+    faClass: "fab fa-angular",
+  },
+  {
+    name: "MongoDB",
+    faClass: "fas fa-database",
+  },
+  {
+    name: "AWS",
+    faClass: "fab fa-aws",
+  },
+  {
+    name: "Markdown",
+    faClass: "fab fa-markdown",
+  },
+  {
+    name: "Git",
+    faClass: "fab fa-git",
+  },
+  {
+    name: "GitHub",
+    faClass: "fab fa-github",
+  },
+  {
+    name: "React Native",
+    faClass: "fas fa-mobile-alt",
+  },
+  {
+    name: "Socket.io",
+    faClass: "far fa-comments",
+  },
+  {
+    name: "iOS",
+    faClass: "fas fa-laptop-code",
+  },
+  {
+    name: "Windows",
+    faClass: "far fa-keyboard",
+  },
+  {
+    name: "DSA",
+    faClass: "fas fa-code",
+  },
+];
+// Edit your projects, its name, your skills used to make it, and the url.
+// You can omit freely anything if you dont have it
+// To Add a Project, copy any one below and paste it after the last comma and increment the id's project number
+const section3Title = "Past Projects";
+const projects = [
+  {
+    // Add image in './styles/images.css' in #project1
+    id: "project1",
+    name: "Tedashi Trained",
+    skills: ["Node, React, MongoDB, SASS, CSS3, Bootstrap"],
+    url: "https://github.com/myerstina515/tedashitrained",
+  },
+  {
+    // Add image in './styles/images.css' in #project2
+    id: "project2",
+    name: "Harry Potter Trivia",
+    skills: ["React, Node, MongoDB, SASS, Material-UI, CSS3"],
+    url: "https://github.com/Granger-Danger2021/trivia",
+  },
+  {
+    // Add image in './styles/images.css' in #project3
+    id: "project3",
+    name: "Shoppies",
+    skills: ["React, CSS3, Bootstrap"],
+    url: "https://github.com/myerstina515/shoppies",
+  },
+  {
+    // Add image in './styles/images.css' in #project4
+    id: "project4",
+    name: "Project 4",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project5
+    id: "project5",
+    name: "Project 5",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project6
+    id: "project6",
+    name: "Parent Pick-up App",
+    skills: ["Socket.io, Node, MongoDB, React, Redux,"],
+    url: "https://github.com/parent-pickup-coordinator",
+  },
+];
+// Edit your Miscellaneous Activities, its name and the url.
+// You can omit freely anything if you dont have it
+// To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
+const section4Title = "Miscellaneous";
+const miscellaneous = [
+  {
+    // Add image in './styles/images.css' in #misc1
+    id: "misc1",
+    name: "School Project: ",
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #misc2
+    id: "misc2",
+    name: "Miscellaneous 2",
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #misc3
+    id: "misc3",
+    name: "Miscellaneous 3",
+    url: "https://github.com/kaustubhai",
+  },
+];
+// Contact form text, and Jotforms link
+// To get your own jotform link, go to https://www.jotform.com/
+// If you hacve the link already, paste it in the contactUrl below
+const section5Title = "Get in Touch";
+const contact = {
+  pitch:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.",
+  copyright: "Tina Myers",
+  contactUrl: "https://formspree.io/f/mwkabqpy",
+};
+// Paste your respective social media links. You can omit any if you dont have it
+// Upload your resume in your drive, get the shaareable link and paste it in the resume section
+const social = {
+  github: "https://github.com/myerstina515",
+  linkedin: "http://linkedin.com/in/tinalmyers",
+  resume: "https://docs.google.com/document/d/1QrYh8ivNetQ7v6kLGgoCxRTN-LuWp2YNBgPpFbaOt4U/edit?usp=sharing",
+};
+// Dont change anything here
+export {
+  animation,
+  header,
+  background,
+  about,
+  skillsBar,
+  projects,
+  miscellaneous,
+  contact,
+  social,
+  section2title,
+  section3Title,
+  section4Title,
+  section5Title,
+};
